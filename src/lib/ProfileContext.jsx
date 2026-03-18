1dfd871587cedcfb255cd99e79d0fe734e351404
vexny gomez <vexny.com@gmail.com>
2026-03-18 00:10:48 +0000

    File changes

--- src/lib/ProfileContext.jsx
+++ src/lib/ProfileContext.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { base44 } from '@/api/base44Client';
+import { queryClientInstance } from '@/lib/query-client';
 import {
   TrendingUp, Plane, Rocket, Zap,
   LayoutDashboard, FolderKanban, CheckSquare, StickyNote,
